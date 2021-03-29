7b96b100a6e36dee6f9588666f23e7ac2e71f189
Vesa Nieminen <vesa.nieminen@gmail.com>
2021-03-29 22:43:59 +0300

    Tested the new input system.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
@@ -12,15 +13,30 @@ public class Movement : MonoBehaviour
     private float movement;
     private bool jump;
 
+
     // Update is called once per frame
     void Update()
     {
-        movement = Input.GetAxis("Horizontal" + playerNumber) * moveSpeed;
+        /*movement = Input.GetAxis("Horizontal" + playerNumber) * moveSpeed;
         if (Input.GetButtonDown("Jump" + playerNumber))
         {
             jump = true;
-        }
+        }*/
+
     }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>().x * moveSpeed;
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        jump = context.ReadValue<float>() > 0;
+        Debug.Log("pressed jump: "+ jump);
+    }
+
+    //public void OnMove(InputAction.CallbackContext context) => movement = context.ReadValue<Vector2>().x;
 
     private void FixedUpdate()
     {
