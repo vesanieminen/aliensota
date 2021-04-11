f8146a85fc245e3043b8f0f053f0845292b49ca5
Vesa Nieminen <vesa.nieminen@gmail.com>
2021-04-11 15:02:46 +0300

    Rigidbody optimization for bodies

--- Assets/Scripts/Body.cs
+++ Assets/Scripts/Body.cs
@@ -6,19 +6,21 @@ public class Body : MonoBehaviour
 {
 
     private Rigidbody2D rigidbody;
+    private float stopTime = 3f;
+    private float rigidTime;
 
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        rigidTime = Time.time + stopTime;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (rigidbody.velocity.magnitude <= 0.01f)
+        if (rigidbody.velocity.magnitude == 0f && Time.time > rigidTime)
         {
             rigidbody.isKinematic = true;
-            Debug.Log("set kinematic: " + true);
         }
     }
 }
