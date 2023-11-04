4b61fd8a71333ee9ea01580578b5f83ef0760813
Vesa Nieminen <vesa@vaadin.com>
2023-11-04 15:54:33 +0200

    Added spike

---
+++ Assets/Scripts/Spike.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Movement player = collision.collider.GetComponent<Movement>();
+        if (player != null)
+        {
+            player.Die();
+        }
+    }
+
+}
